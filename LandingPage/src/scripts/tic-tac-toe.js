776faf6d9984e736209f0c8c276b00b8e9770d91
kdolan28 <kyledolan83@gmail.com>
2019-11-07 05:24:08 -0600

    started tic-tac-toe

---
+++ LandingPage/src/scripts/tic-tac-toe.js
@@ -0,0 +1,13 @@
+var gamemarker = "X";
+
+function changeMarkerToX() {
+  document.getElementById("btn-x").innerHTML = "X";
+  var gamemarker = "X";
+  console.log("The x button was clicked!");
+}
+
+function changeMarkerToO() {
+  document.getElementById("btn-o").innerHTML = "O";
+  var gamemarker = "O";
+  console.log("The o button was clicked!");
+}
