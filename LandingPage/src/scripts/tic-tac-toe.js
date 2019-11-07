04507f1d4f334526c5d01d90a463f15db9b4fb4b
kdolan28 <kyledolan83@gmail.com>
2019-11-07 15:51:30 -0600

    i dont get it

--- LandingPage/src/scripts/tic-tac-toe.js
+++ LandingPage/src/scripts/tic-tac-toe.js
@@ -1,13 +1,17 @@
-var gamemarker = "X";
+var gameMarker = "x";
 
 function changeMarkerToX() {
-  document.getElementById("btn-x").innerHTML = "X";
-  var gamemarker = "X";
+  gameMarker = document.getElementById("btn-x").innerHTML = "X";
+  var gameMarker = "X";
   console.log("The x button was clicked!");
 }
 
 function changeMarkerToO() {
-  document.getElementById("btn-o").innerHTML = "O";
-  var gamemarker = "O";
+  gameMarker = document.getElementById("btn-o").innerHTML = "O";
+  var gameMarker = "O";
   console.log("The o button was clicked!");
+}
+
+function placeMark() {
+  document.getElementById("q0").innerHTML = gameMarker;
 }
