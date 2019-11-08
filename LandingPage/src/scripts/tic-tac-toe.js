9e4255d5cc314779a09fa836ceabbce8762b1347
kdolan28 <kyledolan83@gmail.com>
2019-11-07 20:02:39 -0600

    thursday

--- LandingPage/src/scripts/tic-tac-toe.js
+++ LandingPage/src/scripts/tic-tac-toe.js
@@ -1,13 +1,13 @@
 var gameMarker = "x";
 
 function changeMarkerToX() {
-  gameMarker = document.getElementById("btn-x").innerHTML = "X";
+  document.getElementById("btn-x").innerHTML = "X";
   var gameMarker = "X";
   console.log("The x button was clicked!");
 }
 
 function changeMarkerToO() {
-  gameMarker = document.getElementById("btn-o").innerHTML = "O";
+  document.getElementById("btn-o").innerHTML = "O";
   var gameMarker = "O";
   console.log("The o button was clicked!");
 }
